0fb3691ba0bb02aa045e04d1bb5c5f1df47e4d21
bayamax <bayamax@icloud.com>
2024-06-07 23:34:36 +0900

    heroku用マイグレーションし直し

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -91,7 +91,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Database
+#Database
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
